cddf6644c10aea0f119b1b9bbee6af599f6b4444
IainMertner <iain@mertner.com>
2025-12-23 22:46:57 +0000

    Rename main.py

--- main.py
+++ main.py
@@ -1,35 +1,78 @@
 import numpy as np
+import pygame
+import pygame._sdl2 as sdl2
 
 from utils.load_signals import load_signals
+from utils.scroll_buffer import ScrollBuffer
 from utils.get_sigma import get_sigma
+from utils.generators import ARGenerator, EnvelopeGenerator, ThicknessGenerator
 from train_ar_model import train_ar_model
-from fit_ar1_residuals import compute_residuals, fit_ar1_residuals
-from generate_series import generate_series, process_series
-from visualise import render_line_image, next_available_filename, save_image
+from utils.envelopes import extract_envelope
+from generate_col import ColumnGenerator
 
 signals = load_signals()
 
 P = 50
-length = 2000
+HEIGHT = 300
+WIDTH = 1200
+MAX_STEP = 2.0
+MAX_ACCEL = 0.25
+
+# get amplitude from training data
+amp = get_sigma()
 
 ## fit autoregressive model of order p
 ar_model = train_ar_model(signals, P)
 phi = np.array(ar_model["phi"])
+noise_std = ar_model["noise_std"]
 
-## compute residuals
-residuals = compute_residuals(signals, phi)
-## fit AR(1) model to residuals
-ar1_residuals_model = fit_ar1_residuals(residuals)
-alpha = np.array(ar1_residuals_model["alpha"])
-sigma_eta = ar1_residuals_model["sigma_eta"]
+# get envelopes
+envelopes = [extract_envelope(signal, sigma=130) for signal in signals]
 
-sigma = get_sigma()
+## scrolling buffer to hold generated image
+buffer = ScrollBuffer(HEIGHT, WIDTH)
 
-## generate synthetic series
-generated_series_norm = generate_series(phi, alpha, sigma_eta, length=length)
-## process generated series
-generated_series = process_series(signals, generated_series_norm, sigma)
+## generators
+ar_gen = ARGenerator(phi, noise_std)
+env_gen = EnvelopeGenerator(envelopes)
+thick_gen = ThicknessGenerator()
+col_gen = ColumnGenerator(ar_gen, env_gen, thick_gen, HEIGHT, amp, MAX_STEP)
 
-## visualise generated series
-img = render_line_image(generated_series)
-save_image(img)
+### run generation loop and render
+pygame.init()
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Heartbeat")
+
+window = sdl2.Window.from_display_module()
+window.maximize()
+
+clock = pygame.time.Clock()
+
+running = True
+## main loop
+while running:
+    for event in pygame.event.get():
+        # quit event
+        if event.type == pygame.QUIT:
+            running = False
+        # resize event
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            buffer.resize_width(event.w)
+    # generate next column
+    col = col_gen.step()
+    # append to buffer
+    buffer.append_col(col)
+    # convert to rgb
+    rgb = np.stack([buffer.img]*3, axis=-1)
+    ## render to screen
+    surf = pygame.surfarray.make_surface(rgb.swapaxes(0,1))
+    screen.fill((255, 255, 255))
+    y_offset = (screen.get_height() - buffer.img.shape[0]) // 2
+    screen.blit(surf, (0, y_offset))
+    pygame.display.flip()
+    # frame rate limit
+    clock.tick(300)
+
+pygame.quit()
---
+++ main_static.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from utils.load_signals import load_signals
+from utils.get_sigma import get_sigma
+from train_ar_model import train_ar_model
+from fit_ar1_residuals import compute_residuals, fit_ar1_residuals
+from generate_series import generate_series, process_series
+from visualise import render_line_image, next_available_filename, save_image
+
+signals = load_signals()
+
+P = 50
+length = 2000
+
+## fit autoregressive model of order p
+ar_model = train_ar_model(signals, P)
+phi = np.array(ar_model["phi"])
+
+## compute residuals
+residuals = compute_residuals(signals, phi)
+## fit AR(1) model to residuals
+ar1_residuals_model = fit_ar1_residuals(residuals)
+alpha = np.array(ar1_residuals_model["alpha"])
+sigma_eta = ar1_residuals_model["sigma_eta"]
+
+sigma = get_sigma()
+
+## generate synthetic series
+generated_series_norm = generate_series(phi, alpha, sigma_eta, length=length)
+## process generated series
+generated_series = process_series(signals, generated_series_norm, sigma)
+
+## visualise generated series
+img = render_line_image(generated_series)
+save_image(img)

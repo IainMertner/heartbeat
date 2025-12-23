ca21ea2a120b18bec2b2c6b772ef6cd9a93f88fd
IainMertner <iain@mertner.com>
2025-12-23 22:44:03 +0000

    Dynamic window resizing

--- main_scroll.py
+++ main_scroll.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pygame
-import time
+import pygame._sdl2 as sdl2
 
 from utils.load_signals import load_signals
 from utils.scroll_buffer import ScrollBuffer
@@ -15,8 +15,8 @@ signals = load_signals()
 P = 50
 HEIGHT = 300
 WIDTH = 1200
-MAX_STEP = 2.5
-MAX_ACCEL = 0.5
+MAX_STEP = 2.0
+MAX_ACCEL = 0.25
 
 # get amplitude from training data
 amp = get_sigma()
@@ -42,6 +42,11 @@ col_gen = ColumnGenerator(ar_gen, env_gen, thick_gen, HEIGHT, amp, MAX_STEP)
 pygame.init()
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+pygame.display.set_caption("Heartbeat")
+
+window = sdl2.Window.from_display_module()
+window.maximize()
+
 clock = pygame.time.Clock()
 
 running = True
@@ -51,15 +56,21 @@ while running:
         # quit event
         if event.type == pygame.QUIT:
             running = False
+        # resize event
+        elif event.type == pygame.VIDEORESIZE:
+            screen = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+            buffer.resize_width(event.w)
     # generate next column
     col = col_gen.step()
     # append to buffer
     buffer.append_col(col)
     # convert to rgb
     rgb = np.stack([buffer.img]*3, axis=-1)
-    # render to screen
+    ## render to screen
     surf = pygame.surfarray.make_surface(rgb.swapaxes(0,1))
-    screen.blit(surf, (0,0))
+    screen.fill((255, 255, 255))
+    y_offset = (screen.get_height() - buffer.img.shape[0]) // 2
+    screen.blit(surf, (0, y_offset))
     pygame.display.flip()
     # frame rate limit
     clock.tick(300)
--- utils/scroll_buffer.py
+++ utils/scroll_buffer.py
@@ -9,3 +9,15 @@ class ScrollBuffer:
     def append_col(self, col):
         self.img[:, :-1] = self.img[:, 1:]
         self.img[:, -1] = col
+
+    def resize_width(self, new_width):
+        h, w = self.img.shape
+        new = np.full((h, new_width), 255, dtype=np.uint8)
+        # crop from the right (keep most recent data) if smaller
+        if new_width <= w:
+            new[:] = self.img[:, w - new_width : w]
+        # otherwise, pad on the left
+        else:
+            new[:, new_width - w:] = self.img
+        
+        self.img = new
